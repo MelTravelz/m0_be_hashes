32f72c9bdd61e998d6566c5f53dce9e2005ce418
Melony Franchini <meltravelz@gmail.com>
2022-11-03 16:01:28 -0600

    Add syntax and half hashes docs

--- hashes_1.rb
+++ hashes_1.rb
@@ -1,29 +1,35 @@
 
-Activity:
+# Activity:
 
 # In the below exercises, write code that achieves
 # the desired result. To check your work, run this
 # file by entering the following command in your Terminal: 
 # `ruby hashes_1.rb`
 
-foods = {"apples": 23, "grapes": 507, "eggs": 48}
-p zoo
+foods = {
+  apples: 23, 
+  grapes: 507, 
+  eggs: 48
+}
+
+p foods
 
 # Write code that prints all of the 'keys' of the foods variable 
 # you created above:
-# YOUR CODE HERE
+p foods.keys
 
 # Write code that prints all of the 'values' of the foods variable 
 # you created above:
-# YOUR CODE HERE
+p foods.values
 
 # Write code that prints the value of the second food of the foods variable 
 # you created above:
-# YOUR CODE HERE
+p foods.values[1]
 
 # Write code that adds a food to the foods hash. 
 # Then, print the updated hash:
-# YOUR CODE HERE
+p foods["onions"] = 12
+p foods
 
 
 #-------------------
@@ -38,15 +44,25 @@ p zoo
 # value should be some appropriate value for that key. Work to have at least 5 key-value pairs.
 
 # Write code that prints your email hash to the terminal.
+email = {
+  from: "MelTravelz@gmail.com",
+  to: "mybestfriend@gmail.com",
+  date: "Nov. 3, 2022",
+  text: "Hey, It's been a while. Let's catch up. Call me! Mel",
+  attachments: false, 
+  bbc: false
+}
+
+p email
 
 
 # Write code that prints all of the 'keys' of the email hash 
 # you created above:
-# YOUR CODE HERE
+p email.keys
 
 # Write code that prints all of the 'values' of the email hash 
 # you created above:
-# YOUR CODE HERE
+p email.values
 
 
 #-------------------
